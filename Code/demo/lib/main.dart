b379f2b9eda3979db92abbc6a177d1403f7fcf3a
lafenebp <90349278+lafenebp@users.noreply.github.com>
2021-11-07 10:38:36 -0500

    restructured so hot reload works

--- Code/demo/lib/main.dart
+++ Code/demo/lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.deepPurple[200],
         appBar: AppBar(
-          title: const Text('Spencer Stinks!'),
+          title: const Text('Spencer Stinks!!!'),
           backgroundColor: Colors.red[600],
         ),
         body: const Center(
           child: Text('So does Sam! XDDDDD'),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
