0ae9911db7f1b33c6917f034ae69a62ce5db0714
Spencer Snider <snidersc@mail.uc.edu>
2022-02-02 23:56:47 -0500

    mongo_intro: This doesnt work, but might be useful to have later.

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,50 +1,5 @@
-import 'package:demo/pages/profile_creation_intro.dart';
-import 'package:demo/pages/task_creation.dart';
-import 'package:demo/themes/default_theme.dart';
-import 'package:flutter/material.dart';
+import 'package:demo/server.dart' as server;
 
-void main() {
-  runApp(MaterialApp(
-    theme: DefaultTheme.defaultTheme,
-    title: 'Landing Page',
-    home: const CoreRunning(),
-  ));
-}
-
-class CoreRunning extends StatelessWidget {
-  const CoreRunning({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: const Text('Create Life : o )'),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const TaskCreation()));
-              },
-            ),
-            const SizedBox(
-              width: 200,
-              height: 100,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const ProfileCreationIntro()));
-                },
-                child: const Text('Control Life ;-;'))
-          ],
-        ),
-      ),
-    );
-  }
+void main(List<String> arguments) {
+  server.start();
 }
