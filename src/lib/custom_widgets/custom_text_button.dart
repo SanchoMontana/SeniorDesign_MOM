ee9e4527a1836baec5f5ce7def6b46e0988dff15
lafenebp <90349278+lafenebp@users.noreply.github.com>
2022-02-08 13:55:00 -0500

    adding some form validation

--- src/lib/custom_widgets/custom_text_button.dart
+++ src/lib/custom_widgets/custom_text_button.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 SizedBox customTextButton(String buttonText, void Function()? onPressed,
-    {double width = 140}) {
+    {double width = 140, Key? k}) {
   return SizedBox(
     width: width,
     child: TextButton(
       onPressed: onPressed,
-      child: Text(buttonText),
+      child: Text(buttonText, key: k),
     ),
   );
 }
--- src/lib/pages/task_creation.dart
+++ src/lib/pages/task_creation.dart
@@ -1,7 +1,22 @@
 import 'package:demo/custom_widgets/custom_text_button.dart';
 import 'package:flutter/material.dart';
-import 'package:demo/custom_widgets/day_picker_button.dart';
+import 'package:demo/custom_widgets/number_picker.dart';
 import 'package:demo/themes/constants.dart';
+
+final List<DayPickerButton> dayButtons = [
+  DayPickerButton('SU'),
+  DayPickerButton('MO'),
+  DayPickerButton('TU'),
+  DayPickerButton('WE'),
+  DayPickerButton('TH'),
+  DayPickerButton('FR'),
+  DayPickerButton('SA'),
+];
+
+bool daysPressed = false;
+bool timesPressed = false;
+bool buildPressed = false;
+bool breakPressed = false;
 
 class TaskCreation extends StatefulWidget {
   const TaskCreation({Key? key}) : super(key: key);
@@ -11,10 +26,7 @@ class TaskCreation extends StatefulWidget {
 }
 
 class _TaskCreationState extends State<TaskCreation> {
-  bool daysPressed = false;
-  bool timesPressed = false;
-  bool buildPressed = false;
-  bool breakPressed = false;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -27,124 +39,166 @@ class _TaskCreationState extends State<TaskCreation> {
             SizedBox(
               width: 350,
               child: Center(
-                child: Card(
-                  color: Colors.blueGrey[500],
-                  child: Column(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: Text("Create A Task"),
-                      ),
-                      const SizedBox(
-                        height: 30,
-                        width: 300,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Task Name',
+                child: Form(
+                  key: _formKey,
+                  child: Card(
+                    child: Column(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: Text("Create A Task"),
+                        ),
+                        SizedBox(
+                          width: 300,
+                          child: TextFormField(
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please name your task';
+                              } else {
+                                return null;
+                              }
+                            },
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              isDense: true,
+                              labelText: 'Task Name',
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 10, horizontal: 10),
+                            ),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 20),
-                      const Text("Do you want to build or break your habit?"),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          SizedBox(
-                            width: 140,
-                            child: TextButton(
-                              style: buildPressed
-                                  ? kTextButtonSelectedTheme
-                                  : null,
-                              child: const Text("Build 👷‍♂️"),
-                              onPressed: () {
-                                setState(() {
-                                  buildPressed = true;
-                                  breakPressed = false;
-                                });
+                        const SizedBox(height: 20),
+                        const Text("Do you want to build or break your habit?"),
+                        FormField(
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (value) {
+                            if (!buildPressed && !breakPressed) {
+                              return 'Please select build or break';
+                            } else {
+                              return null;
+                            }
+                          },
+                          builder: (FormFieldState state) {
+                            return Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    SizedBox(
+                                      width: 140,
+                                      child: TextButton(
+                                        style: buildPressed
+                                            ? kTextButtonSelectedTheme
+                                            : null,
+                                        child: const Text("Build 👷‍♂️"),
+                                        onPressed: () {
+                                          setState(() {
+                                            buildPressed = true;
+                                            breakPressed = false;
+                                            state.validate();
+                                          });
+                                        },
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 140,
+                                      child: TextButton(
+                                        style: breakPressed
+                                            ? kTextButtonSelectedTheme
+                                            : null,
+                                        child: const Text("Break 🔨"),
+                                        onPressed: () {
+                                          setState(() {
+                                            buildPressed = false;
+                                            breakPressed = true;
+                                            state.validate();
+                                          });
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                if (state.hasError) ...[
+                                  Text(state.errorText!,
+                                      style: kErrorTextStyle),
+                                ]
+                              ],
+                            );
+                          },
+                        ),
+                        const SizedBox(height: 20),
+                        const Text("How do you want to be reminded?"),
+                        FormField(
+                          builder: (FormFieldState state) {
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SizedBox(
+                                  width: 140,
+                                  child: TextButton(
+                                    style: daysPressed
+                                        ? kTextButtonSelectedTheme
+                                        : null,
+                                    child: const Text("Certain Days"),
+                                    onPressed: () {
+                                      setState(() {
+                                        daysPressed = true;
+                                        timesPressed = false;
+                                      });
+                                    },
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 140,
+                                  child: TextButton(
+                                    style: timesPressed
+                                        ? kTextButtonSelectedTheme
+                                        : null,
+                                    child: const Text("Certain # of Times"),
+                                    onPressed: () {
+                                      setState(() {
+                                        daysPressed = false;
+                                        timesPressed = true;
+                                      });
+                                    },
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                        daysPressed
+                            ? DayPicker()
+                            : timesPressed
+                                ? const NumberPicker()
+                                : Container(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            customTextButton(
+                              'Cancel',
+                              () {
+                                // ignore: avoid_print
+                                print('cancelling :(');
                               },
+                              width: 140,
                             ),
-                          ),
-                          SizedBox(
-                            width: 140,
-                            child: TextButton(
-                              style: breakPressed
-                                  ? kTextButtonSelectedTheme
-                                  : null,
-                              child: const Text("Break 🔨"),
-                              onPressed: () {
-                                setState(() {
-                                  buildPressed = false;
-                                  breakPressed = true;
-                                });
+                            customTextButton(
+                              'Submit', //TODO: turn this into a form and don't let them hit submit until enough is selected
+                              () {
+                                if (_formKey.currentState!.validate()) {
+                                  print('form is good :o)');
+                                }
                               },
+                              width: 140,
                             ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 20),
-                      const Text("How do you want to be reminded?"),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          SizedBox(
-                            width: 140,
-                            child: TextButton(
-                              style:
-                                  daysPressed ? kTextButtonSelectedTheme : null,
-                              child: const Text("Certain Days"),
-                              onPressed: () {
-                                setState(() {
-                                  daysPressed = true;
-                                  timesPressed = false;
-                                });
-                              },
-                            ),
-                          ),
-                          SizedBox(
-                            width: 140,
-                            child: TextButton(
-                              style: timesPressed
-                                  ? kTextButtonSelectedTheme
-                                  : null,
-                              child: const Text("Certain # of Times"),
-                              onPressed: () {
-                                setState(() {
-                                  daysPressed = false;
-                                  timesPressed = true;
-                                });
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                      daysPressed
-                          ? const DayPicker()
-                          : timesPressed
-                              ? const NumberPicker()
-                              : Container(),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          customTextButton(
-                            'Cancel',
-                            () {
-                              // ignore: avoid_print
-                              print('cancelling :(');
-                            },
-                            width: 140,
-                          ),
-                          customTextButton(
-                            'Submit', //TODO: turn this into a form and don't let them hit submit until enough is selected
-                            () {
-                              // ignore: avoid_print
-                              print('submitting :D');
-                            },
-                            width: 140,
-                          ),
-                        ],
-                      )
-                    ],
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -156,12 +210,8 @@ class _TaskCreationState extends State<TaskCreation> {
   }
 }
 
-//for the submit button, you could see if you can just enable or disable it thru code, and just make sure that name has a value, the buttons are selected, and days or # is selected. just a giant if-condition
-
-//daypicker widget with text as constructor parameter. define theme variable, when it's clicked call a changeBackground method that updates the theme? with onPressed and setState?
-
 class DayPicker extends StatefulWidget {
-  const DayPicker({Key? key}) : super(key: key);
+  DayPicker({Key? key}) : super(key: key);
 
   @override
   _DayPickerState createState() => _DayPickerState();
@@ -178,15 +228,7 @@ class _DayPickerState extends State<DayPicker> {
         const SizedBox(height: 10),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: const [
-            DayPickerButton('Su'),
-            DayPickerButton('Mo'),
-            DayPickerButton('Tu'),
-            DayPickerButton('We'),
-            DayPickerButton('Th'),
-            DayPickerButton('Fr'),
-            DayPickerButton('Su'),
-          ],
+          children: dayButtons,
         ),
         const SizedBox(height: 20),
       ],
@@ -194,82 +236,37 @@ class _DayPickerState extends State<DayPicker> {
   }
 }
 
-class NumberPicker extends StatefulWidget {
-  const NumberPicker({Key? key}) : super(key: key);
+class DayPickerButton extends StatefulWidget {
+  final String btnText;
+
+  bool btnSelected = false;
+
+  DayPickerButton(this.btnText, {Key? key}) : super(key: key);
 
   @override
-  _NumberPickerState createState() => _NumberPickerState();
+  _DayPickerButtonState createState() => _DayPickerButtonState();
 }
 
-class _NumberPickerState extends State<NumberPicker> {
-  int count = 1;
-
+class _DayPickerButtonState extends State<DayPickerButton> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  if (count > 1) {
-                    count--;
-                  }
-                });
-              },
-              child: Container(
-                width: 25,
-                height: 25,
-                decoration: BoxDecoration(
-                  color: kTextButtonColor,
-                  shape: BoxShape.circle,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: const [
-                    Icon(Icons.remove, size: 25),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(width: 10),
-            Text(
-              count.toString(),
-              style: const TextStyle(fontSize: 35),
-            ),
-            const SizedBox(width: 10),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  if (count < 7) {
-                    count++;
-                  }
-                });
-              },
-              child: Container(
-                width: 25,
-                height: 25,
-                decoration: BoxDecoration(
-                  color: kTextButtonColor,
-                  shape: BoxShape.circle,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: const [
-                    Icon(Icons.add, size: 25),
-                  ],
-                ),
-              ),
-            ),
-          ],
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          widget.btnSelected = !widget.btnSelected;
+        });
+      },
+      child: Container(
+        width: 30,
+        height: 30,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color:
+              widget.btnSelected ? kTextButtonSelectedColor : kTextButtonColor,
+          shape: BoxShape.circle,
         ),
-        const SizedBox(height: 20),
-      ],
+        child: Text(widget.btnText),
+      ),
     );
   }
 }
