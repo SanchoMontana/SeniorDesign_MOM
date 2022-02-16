e9b03abe1394297080a6448821450d1d897348f1
Spencer Snider <snidersc@mail.uc.edu>
2022-02-16 12:57:31 -0500

    master: merged in homePage.

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,55 +1,28 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// ignore_for_file: import_of_legacy_library_into_null_safe
 import 'package:firebase_core/firebase_core.dart';
-import 'package:demo/pages/profile_creation_intro.dart';
 import 'package:demo/pages/task_creation.dart';
 import 'package:flutter/material.dart';
 import 'package:demo/themes/constants.dart';
+import 'package:demo/pages/home.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MaterialApp(
-    theme: kDefaultTheme,
-    title: 'Landing Page',
-    home: const CoreRunning(),
-  ));
+  runApp(Unblockd());
 }
 
-class CoreRunning extends StatelessWidget {
-  const CoreRunning({Key? key}) : super(key: key);
-
+class Unblockd extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: const Text('Create Life : o )'),
-              onPressed: () => {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const TaskCreation())),
-              },
-            ),
-            const SizedBox(
-              width: 200,
-              height: 100,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const TaskCreation()));
-                },
-                child: const Text('Control Life ;-;'))
-          ],
-        ),
-      ),
+    return MaterialApp(
+      theme: kDefaultTheme,
+      title: 'Landing Page',
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => Home(),
+        'task_creation': (context) => TaskCreation()
+      },
     );
   }
 }
---
+++ src/lib/pages/home.dart
@@ -0,0 +1,44 @@
+import 'package:demo/pages/task.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:demo/pages/task.dart';
+
+class Home extends StatelessWidget {
+
+  List<Widget> today_tasks = [
+    Task(
+      task_name: "workout time", 
+      time: "3 PM",
+      today: true,
+    ),
+    Task(
+      task_name: "Read newpaper", 
+      time: "6 AM",
+      today: true,
+    ),
+    
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: Stack(
+          children: <Widget>[
+            Image.asset('res/lib_draft.png',
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.fitHeight,
+              alignment: Alignment.topLeft,
+            ),
+            ListView(
+              children: today_tasks
+            )
+            
+          ],
+        )
+      ),
+    );
+  }
+}
