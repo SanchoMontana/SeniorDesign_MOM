2b8d4ac19a2b03880ba4d30c737cb2ce63f967e4
TreeBeard2112 <jansensd@mail.uc.edu>
2022-02-17 10:27:05 -0500

    creation of new branch, inherating a bunch of shit

---
+++ src/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_macos.build/Objects-normal/x86_64/shared_preferences_macos_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/TreeBeard/Code/SeniorDesign/SeniorDesign_MOM/SeniorDesign_MOM/src/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_macos.build/DerivedSources/shared_preferences_macos_vers.c
