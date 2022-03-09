a1523d91900d621973f9810214af6a2667c2f134
Spencer Snider <snidersc@mail.uc.edu>
2022-03-09 12:45:07 -0500

    Notifications: Working at least.

---
+++ src/lib/custom_classes/notification_service.dart
@@ -0,0 +1,201 @@
+// import 'package:demo/pages/home_page.dart';
+// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+// import 'package:flutter/material.dart';
+// import 'package:timezone/data/latest_all.dart' as tz;
+// import 'package:timezone/timezone.dart' as tz;
+
+// class NotificationService {
+//   static final NotificationService _notificationService =
+//       NotificationService._internal();
+
+//   factory NotificationService() {
+//     return _notificationService;
+//   }
+
+//   NotificationService._internal();
+//   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+//       FlutterLocalNotificationsPlugin();
+
+//   Future<Object?> selectNotification(context, String payload) async {
+//     if (payload != null) {
+//       debugPrint('notification payload: $payload');
+//     }
+//     return await Navigator.pushNamed(context, HomePage.id);
+//   }
+
+//   void new_notif() async {
+//     const String groupKey = 'com.android.example.WORK_EMAIL';
+//     const String groupChannelId = 'grouped channel id';
+//     const String groupChannelName = 'grouped channel name';
+//     const String groupChannelDescription = 'grouped channel description';
+//     // example based on https://developer.android.com/training/notify-user/group.html
+
+//     ////// Notification Details //////
+//     const AndroidNotificationDetails firstNotificationAndroidSpecifics =
+//         AndroidNotificationDetails(groupChannelId, groupChannelName,
+//             channelDescription: groupChannelDescription,
+//             importance: Importance.max,
+//             priority: Priority.high,
+//             groupKey: groupKey);
+//     const NotificationDetails firstNotificationPlatformSpecifics =
+//         NotificationDetails(android: firstNotificationAndroidSpecifics);
+
+//     ///// Initalization Settings /////
+//     const AndroidInitializationSettings initializationSettingsAndroid =
+//         AndroidInitializationSettings('app_icon');
+//     final IOSInitializationSettings initializationSettingsIOS =
+//         IOSInitializationSettings();
+//     final MacOSInitializationSettings initializationSettingsMacOS =
+//         MacOSInitializationSettings();
+//     final InitializationSettings initializationSettings =
+//         InitializationSettings(
+//             android: initializationSettingsAndroid,
+//             iOS: initializationSettingsIOS,
+//             macOS: initializationSettingsMacOS);
+
+//     ///// Notificaiton Selection /////
+//     await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+//         onSelectNotification: selectNotification());
+
+//     await flutterLocalNotificationsPlugin.show(1, 'Alex Faarborg',
+//         'You will not believe...', firstNotificationPlatformSpecifics);
+//     const AndroidNotificationDetails secondNotificationAndroidSpecifics =
+//         AndroidNotificationDetails(groupChannelId, groupChannelName,
+//             channelDescription: groupChannelDescription,
+//             importance: Importance.max,
+//             priority: Priority.high,
+//             groupKey: groupKey);
+//     const NotificationDetails secondNotificationPlatformSpecifics =
+//         NotificationDetails(android: secondNotificationAndroidSpecifics);
+//     await flutterLocalNotificationsPlugin.show(
+//         2,
+//         'Jeff Chang',
+//         'Please join us to celebrate the...',
+//         secondNotificationPlatformSpecifics);
+
+// // Create the summary notification to support older devices that pre-date
+//     /// Android 7.0 (API level 24).
+//     ///
+//     /// Recommended to create this regardless as the behaviour may vary as
+//     /// mentioned in https://developer.android.com/training/notify-user/group
+//     const List<String> lines = <String>[
+//       'Alex Faarborg  Check this out',
+//       'Jeff Chang    Launch Party'
+//     ];
+//     const InboxStyleInformation inboxStyleInformation = InboxStyleInformation(
+//         lines,
+//         contentTitle: '2 messages',
+//         summaryText: 'janedoe@example.com');
+//     const AndroidNotificationDetails androidPlatformChannelSpecifics =
+//         AndroidNotificationDetails(groupChannelId, groupChannelName,
+//             channelDescription: groupChannelDescription,
+//             styleInformation: inboxStyleInformation,
+//             groupKey: groupKey,
+//             setAsGroupSummary: true);
+//     const NotificationDetails platformChannelSpecifics =
+//         NotificationDetails(android: androidPlatformChannelSpecifics);
+//     await flutterLocalNotificationsPlugin.show(
+//         3, 'Attention', 'Two messages', platformChannelSpecifics);
+//   }
+
+//   void set_timezone() async {
+//     tz.initializeTimeZones();
+//     tz.setLocalLocation(tz.getLocation("America/Cincinnati"));
+//     await flutterLocalNotificationsPlugin.zonedSchedule(
+//         0,
+//         'scheduled title',
+//         'scheduled body',
+//         tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
+//         const NotificationDetails(
+//             android: AndroidNotificationDetails(
+//                 'your channel id', 'your channel name',
+//                 channelDescription: 'your channel description')),
+//         androidAllowWhileIdle: true,
+//         uiLocalNotificationDateInterpretation:
+//             UILocalNotificationDateInterpretation.absoluteTime);
+//   }
+// }
+
+// void disp(flnp) async {
+//   // Displaying notifications
+//   const AndroidNotificationDetails androidPlatformChannelSpecifics =
+//       AndroidNotificationDetails('your channel id', 'your channel name',
+//           channelDescription: 'your channel description',
+//           importance: Importance.max,
+//           priority: Priority.high,
+//           ticker: 'ticker');
+//   const NotificationDetails platformChannelSpecifics =
+//       NotificationDetails(android: androidPlatformChannelSpecifics);
+//   await flnp.show(0, 'plain title', 'plain body', platformChannelSpecifics,
+//       payload: 'item x');
+// }
+
+// Future<FlutterLocalNotificationsPlugin> notif() async {
+//   const String groupKey = 'com.android.example.WORK_EMAIL';
+//   const String groupChannelId = 'grouped channel id';
+//   const String groupChannelName = 'grouped channel name';
+//   const String groupChannelDescription = 'grouped channel description';
+// // example based on https://developer.android.com/training/notify-user/group.html
+//   const AndroidNotificationDetails firstNotificationAndroidSpecifics =
+//       AndroidNotificationDetails(groupChannelId, groupChannelName,
+//           channelDescription: groupChannelDescription,
+//           importance: Importance.max,
+//           priority: Priority.high,
+//           groupKey: groupKey);
+//   const NotificationDetails firstNotificationPlatformSpecifics =
+//       NotificationDetails(android: firstNotificationAndroidSpecifics);
+
+//   const AndroidInitializationSettings initializationSettingsAndroid =
+//       AndroidInitializationSettings('app_icon');
+//   final IOSInitializationSettings initializationSettingsIOS =
+//       IOSInitializationSettings();
+//   final MacOSInitializationSettings initializationSettingsMacOS =
+//       MacOSInitializationSettings();
+//   final InitializationSettings initializationSettings = InitializationSettings(
+//       android: initializationSettingsAndroid,
+//       iOS: initializationSettingsIOS,
+//       macOS: initializationSettingsMacOS);
+//   await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+//       onSelectNotification: selectNotification);
+
+//   await flutterLocalNotificationsPlugin.show(1, 'Alex Faarborg',
+//       'You will not believe...', firstNotificationPlatformSpecifics);
+//   const AndroidNotificationDetails secondNotificationAndroidSpecifics =
+//       AndroidNotificationDetails(groupChannelId, groupChannelName,
+//           channelDescription: groupChannelDescription,
+//           importance: Importance.max,
+//           priority: Priority.high,
+//           groupKey: groupKey);
+//   const NotificationDetails secondNotificationPlatformSpecifics =
+//       NotificationDetails(android: secondNotificationAndroidSpecifics);
+//   await flutterLocalNotificationsPlugin.show(
+//       2,
+//       'Jeff Chang',
+//       'Please join us to celebrate the...',
+//       secondNotificationPlatformSpecifics);
+
+// // Create the summary notification to support older devices that pre-date
+//   /// Android 7.0 (API level 24).
+//   ///
+//   /// Recommended to create this regardless as the behaviour may vary as
+//   /// mentioned in https://developer.android.com/training/notify-user/group
+//   const List<String> lines = <String>[
+//     'Alex Faarborg  Check this out',
+//     'Jeff Chang    Launch Party'
+//   ];
+//   const InboxStyleInformation inboxStyleInformation = InboxStyleInformation(
+//       lines,
+//       contentTitle: '2 messages',
+//       summaryText: 'janedoe@example.com');
+//   const AndroidNotificationDetails androidPlatformChannelSpecifics =
+//       AndroidNotificationDetails(groupChannelId, groupChannelName,
+//           channelDescription: groupChannelDescription,
+//           styleInformation: inboxStyleInformation,
+//           groupKey: groupKey,
+//           setAsGroupSummary: true);
+//   const NotificationDetails platformChannelSpecifics =
+//       NotificationDetails(android: androidPlatformChannelSpecifics);
+//   await flutterLocalNotificationsPlugin.show(
+//       3, 'Attention', 'Two messages', platformChannelSpecifics);
+//   return flutterLocalNotificationsPlugin;
+// }
